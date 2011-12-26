da19742884e677e262851d25eae169e36736bbb5
c9s <cornelius.howl@gmail.com>
2011-12-26 12:13:37 +0800

    Add ApcClassLoader

---
+++ src/Universal/ClassLoader/ApcClassLoader.php
@@ -0,0 +1,35 @@
+<?php
+namespace Universal\ClassLoader;
+use Exception;
+
+class ApcClassLoader extends SplClassLoader
+{
+    private $_apcNamespace;
+
+    public function __construct($namespaces = null)
+    {
+        parent::__construct( $namespaces );
+        if( ! extension_loaded('apc') )
+            throw new Exception('apc extension is not loaded.');
+    }
+
+    public function setApcNamespace($ns)
+    {
+        $this->_apcNamespace = $ns;
+    }
+
+    public function loadClass($class)
+    {
+        if( ($file = apc_fetch($this->_apcNamespace . '::' . $class) ) !== false ) {
+            require $file;
+            return true;
+        }
+
+        if ($file = $this->findClassFile($class)) {
+            apc_store( $this->_apcNamespace . '::' . $class , $file );
+            require $file;
+            return true;
+        }
+        return false;
+    }
+}
