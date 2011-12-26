7c53f2ed8acc75705393980a013c91d22e7654d9
c9s <cornelius.howl@gmail.com>
2011-12-26 13:12:57 +0800

    implements EventDispatcher

---
+++ src/Universal/Event/EventDispatcher.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * This file is part of the Universal package.
+ *
+ * (c) Yo-An Lin <cornelius.howl@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+namespace Universal\Event;
+
+
+/**
+ * A Simple Event dispatcher
+ */
+class EventDispatcher
+{
+    public $eventPool = array();
+
+    function register($ev,$cb)
+    {
+        if( ! isset($this->eventPool[ $ev ] ) )
+            $this->eventPool[ $ev ] = array();
+        $this->eventPool[ $ev ][] = $cb;
+    }
+
+    function trigger($ev,$args)
+    {
+        if( isset( $this->eventPool[ $ev ] ) ) {
+            foreach( $this->eventPool[ $ev ] as $cb ) {
+                if( call_user_func_array( $cb , $args ) === false )
+                    break;
+            }
+        }
+    }
+
+    static function getInstance()
+    {
+        static $instance;
+        return $instance ? $instance : $instance = new static;
+    }
+
+}
