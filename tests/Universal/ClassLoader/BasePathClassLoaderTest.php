b4491a9d26563e015ed8a344d9ceec198b3f8fef
c9s <cornelius.howl@gmail.com>
2011-12-31 00:43:24 +0800

    Update test code

--- tests/Universal/ClassLoader/BasePathClassLoaderTest.php
+++ tests/Universal/ClassLoader/BasePathClassLoaderTest.php
@@ -9,10 +9,15 @@ class BasePathClassLoaderTest extends PHPUnit_Framework_TestCase
     function testFunc()
     {
         $loader = new BasePathClassLoader( array( 
-            'vendor/pear', 'external_vendor/src'
+            'tests/lib'
         ));
         $loader->register();
         ok( $loader );
+
+        spl_autoload_call( 'Foo\Bar' );
+        ok( class_exists( 'Foo\Bar' ) );
+
+        $loader->unregister();
     }
 }
 
