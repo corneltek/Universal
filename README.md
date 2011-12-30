9e252753e1263072e8c65fd08b23bc572e9f56c0
c9s <cornelius.howl@gmail.com>
2011-12-31 00:33:34 +0800

    Requirement checker

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ Universal is a general proprose PHP library, includes these items beloew
 - ClassLoaders
 - FileUtils
 - HTTPRequest handler
+
+
+## Requirement Checker
+
+    try {
+        $require = new Universal\Requirement\Requirement;
+        $require->extensions( 'apc','mbstring' );
+        $require->classes( 'ClassName' , 'ClassName2' );
+        $require->functions( 'func1' , 'func2' , 'function3' )
+    }
+    catch( RequireExtensionException $e ) {
+
+    }
+    catch( RequireFunctionException $e ) {
+
+    }
+    catch( RequireClassException $e ) {
+
+    }
---
+++ src/Universal/Requirement/Requirement.php
@@ -0,0 +1,57 @@
+<?php 
+namespace Universal\Requirement;
+
+use Exception;
+class RequireExtensionException extends Exception {} 
+class RequireFunctionException extends Exception {}
+class RequireClassException extends Exception {}
+
+/**
+ * Requirement check class
+ *
+ * @code
+ *
+ * try {
+ *  $require = new Universal\Requirement\Requirement;
+ *  $require->extensions( 'apc','mbstring' );
+ *  $require->classes( 'ClassName' , 'ClassName2' );
+ *  $require->functions( 'func1' , 'func2' , 'function3' )
+ * }
+ * catch( RequireExtensionException $e ) {
+ * }
+ * catch( RequireFunctionException $e ) {
+ * }
+ * catch( RequireClassException $e ) {
+ * }
+ *
+ */
+class Requirement
+{
+
+    function extensions()
+    {
+        $extensions = func_get_args();
+        foreach( $extensions as $extensionName ) {
+            if( ! extension_loaded( $extensionName ) )
+                throw new RequireExtensionException( "Extension $extensionName is required" );
+        }
+    }
+
+    function functions()
+    {
+        $functions = func_get_args();
+        foreach( $functions as $function ) {
+            if( ! function_exists( $function ) )
+                throw new RequireFunctionException( "Function $function is required" );
+        }
+    }
+
+    function classes()
+    {
+        $classes = func_get_args();
+        foreach( $classes as $class ) {
+            if( ! class_exists( $class ) )
+                throw new RequireClassException( "Class $class is required" );
+        }
+    }
+}
