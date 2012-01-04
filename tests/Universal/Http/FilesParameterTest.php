cc7588b25d06c488ddd1e793c321ab924dd03b5a
c9s <cornelius.howl@gmail.com>
2012-01-05 00:44:45 +0800

    More tests

--- tests/Universal/Http/FilesParameterTest.php
+++ tests/Universal/Http/FilesParameterTest.php
@@ -13,6 +13,7 @@ class FilesParameterTest extends PHPUnit_Framework_TestCase
             'name' => 'File1',
             'type' => 'text/plain',
             'size' => 100,
+            'tmp_name' => '/tmp/file1',
             'error' => 0
         );
 
@@ -23,6 +24,22 @@ class FilesParameterTest extends PHPUnit_Framework_TestCase
         is( 100, $req->files->uploaded->size );
         is( 'text/plain', $req->files->uploaded->type );
         is( 0, $req->files->uploaded->error );
+    }
+
+    function testFunc2()
+    {
+        $_FILES = array( );
+        $_FILES['uploaded'] = array( 
+            'name' => array( 'File1' , 'File2' ),
+            'type' => array( 'text/plain', 'text/plain'),
+            'size' => array( 100, 200 ),
+            'error' => array( 0 , 0 ),
+            'tmp_name' => array(  '/tmp/file1' , '/tmp/file2' ),
+        );
+
+        $req = new HttpRequest;
+        ok( $req );
+        ok( is_array( $req->files->uploaded ) );
     }
 }
 
