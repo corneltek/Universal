bcc16e2decb0ac9f134a946d3e4d6e477e30a560
c9s <cornelius.howl@gmail.com>
2012-01-04 22:53:49 +0800

    Add HttpRequest class.

---
+++ src/Universal/Http/Parameter.php
@@ -0,0 +1,55 @@
+<?php 
+namespace Universal\Http;
+use ArrayAccess;
+
+class Parameter implements ArrayAccess
+{
+    public $hash;
+
+    public function __construct( & $hash = array() )
+    {
+        $this->hash = $hash;
+    }
+
+    public function has( $name )
+    {
+        return isset( $this->hash[ $name ] );
+    }
+
+    public function __isset( $name )
+    {
+        return isset( $this->hash[ $name ] );
+    }
+
+    public function __get( $name )
+    {
+        return @$this->hash[ $name ];
+    }
+
+    public function __set( $name , $value )
+    {
+        $this->hash[ $name ] = $value;
+    }
+
+
+    public function offsetGet($key)
+    {
+        return $this->hash[ $key ];
+    }
+
+    public function offsetSet($key,$value)
+    {
+        $this->hash[ $key ] = $value;
+    }
+
+    public function offsetExists($key)
+    {
+        return isset( $this->hash[ $key ] );
+    }
+
+    public function offsetUnset($key)
+    {
+        unset( $this->hash[ $key ] );
+    }
+
+}
