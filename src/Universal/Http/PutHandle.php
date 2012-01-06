af1f96e5e5514d4c71b5072b5d7d1f753c664d0b
c9s <cornelius.howl@gmail.com>
2012-01-06 23:33:38 +0800

    Add PutHandle

---
+++ src/Universal/Http/PutHandle.php
@@ -0,0 +1,28 @@
+<?php
+namespace Universal\Http;
+
+/**
+ * $put = new Universal\Http\Put;
+ * $data = $put->read(1024);
+ * $put->close();
+ */
+class PutHandle
+{
+    private $handle;
+
+    function __construct()
+    {
+        $this->handle = fopen('php://input','r');
+    }
+
+    function read($i = 1024)
+    {
+        return fread($this->handle, $i));
+    }
+
+    function close()
+    {
+        fclose($this->handle);
+    }
+
+}
