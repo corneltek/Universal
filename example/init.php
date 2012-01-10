60dbc0be81b90d577dbff6b698e3eefccbe1fc8b
c9s <cornelius.howl@gmail.com>
2012-01-10 18:56:49 +0800

    Move Session classes and examples out

--- example/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('basedir',dirname(__DIR__));
-require basedir . '/src/Universal/ClassLoader/BasePathClassLoader.php';
-use Universal\ClassLoader\BasePathClassLoader;
-$classloader = new BasePathClassLoader(array(basedir . '/src'));
-$classloader->register();
