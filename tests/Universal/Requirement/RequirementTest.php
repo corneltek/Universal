1aa01563abb4b2174ef04f326167cbc10f6cb960
c9s <cornelius.howl@gmail.com>
2012-02-27 14:43:40 +0800

    Fix requirement test

--- tests/Universal/Requirement/RequirementTest.php
+++ tests/Universal/Requirement/RequirementTest.php
@@ -18,9 +18,8 @@ class RequirementTest extends PHPUnit_Framework_TestCase
         # ok( $require->extensions('apc') );
         ok( $require->extensions('curl') );
 
-        ok( $require->php( '5.3.0' ) );
-        ok( $require->php( '5.3.8' ) );
-        ok( $require->php( '5.3' ) );
+        ok( $require->php( '5.0.0' ) );
+        ok( $require->php( '5.0' ) );
     }
 }
 
