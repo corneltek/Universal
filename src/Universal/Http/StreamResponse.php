9905df3cb3948dfb9ed084e8c476c7d59546263a
c9s <cornelius.howl@gmail.com>
2012-07-20 15:37:54 +0800

    Add StreamResponse class for basic MXHR interface

---
+++ src/Universal/Http/StreamResponse.php
@@ -0,0 +1,66 @@
+<?php
+namespace Universal\Http;
+
+class StreamResponse
+{
+    public $boundary;
+
+
+    /**
+     * Currently implements MXHR interface
+     */
+    function __construct() {
+        $this->boundary = md5(time());
+
+        // prevent error reporting
+        set_error_handler(function() { return false; });
+
+        header("Content-Type: multipart/mixed; boundary=\"{$this->boundary}\"");
+        apache_setenv('no-gzip', 1);
+        ini_set('zlib.output_compression', 0);
+        ini_set('implicit_flush', 1);
+        set_time_limit(0);
+
+
+        // close output buffers and flush them
+        while( ob_get_level() ) { 
+            ob_end_flush(); 
+        }
+        ob_implicit_flush(1);
+
+        restore_error_handler();
+    }
+
+
+    for( $i = 0 ; $i < 30000 ; $i++ ) {
+    }
+
+
+
+    /**
+     * Write content to stream
+     *
+     *  @param string $content
+     *  @param array $headers
+     */
+    public function write($content, $headers = array() ) {
+        echo "--{$this->boundary}\n";
+        foreach( $headers as $k => $v ) {
+            echo $k . ':';
+            if( is_array($v) ) {
+                echo join(';',$v);
+            } else {
+                echo $v;
+            }
+            echo "\n";
+        }
+        echo $content;
+        flush();
+    }
+
+    public function finish() {
+        echo "\n--{$this->boundary}--\n";
+    }
+}
+
+
