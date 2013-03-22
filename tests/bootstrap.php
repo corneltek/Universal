06dbbc3640c88891905a4ea36235a2f58a23fa99
c9s <cornelius.howl@gmail.com>
2013-03-22 10:12:10 +0800

    Fix travis tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,2 @@
 <?php
-require 'tests/helpers.php';
-require 'src/Universal/ClassLoader/SplClassLoader.php';
-$loader = new \Universal\ClassLoader\SplClassLoader( array(  
-    'Universal' => 'src'
-));
-$loader->register();
+require 'vendor/autoload.php';
