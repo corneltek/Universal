97472961ef44d8de3deef4acc1efb9e7636a1753
c9s <yoanlin93@gmail.com>
2015-07-07 18:43:27 +0800

    extension test case

--- tests/Universal/Http/UploadedFileTest.php
+++ tests/Universal/Http/UploadedFileTest.php
@@ -3,6 +3,41 @@ use Universal\Http\UploadedFile;
 
 class UploadedFileTest extends PHPUnit_Framework_TestCase
 {
+
+    public function testValidateExtension()
+    {
+        $tmpfile = tempnam('/tmp', 'test_');
+        $this->assertNotFalse(file_put_contents($tmpfile, 'foo'));
+        $filestash = array(
+            'name' => 'filename.txt',
+            'tmp_name' => $tmpfile,
+            'type' => 'text/plain',
+            'size' => filesize($tmpfile),
+            'error' => 0,
+        );
+        $file = UploadedFile::createFromArray($filestash);
+        $this->assertNotNull($file);
+        $this->assertTrue($file->validateExtension(['txt']));
+        $this->assertFalse($file->validateExtension(['jpg']));
+    }
+
+    public function testGetExtension()
+    {
+        $tmpfile = tempnam('/tmp', 'test_');
+        $this->assertNotFalse(file_put_contents($tmpfile, 'foo'));
+        $filestash = array(
+            'name' => 'filename.txt',
+            'tmp_name' => $tmpfile,
+            'type' => 'text/plain',
+            'size' => filesize($tmpfile),
+            'error' => 0,
+        );
+        $file = UploadedFile::createFromArray($filestash);
+        $this->assertNotNull($file);
+        $this->assertEquals('txt', $file->getExtension());
+    }
+
+
     public function testMove()
     {
         $tmpfile = tempnam('/tmp', 'test_');
