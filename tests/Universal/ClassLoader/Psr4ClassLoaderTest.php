20e3c4834d0db5754acd11c62fddd1e0881e0f5a
c9s <yoanlin93@gmail.com>
2015-07-14 21:04:31 +0800

    Move fixtures

--- tests/Universal/ClassLoader/Psr4ClassLoaderTest.php
+++ tests/Universal/ClassLoader/Psr4ClassLoaderTest.php
@@ -6,7 +6,7 @@ class Psr4ClassLoaderTest extends PHPUnit_Framework_TestCase
     public function testPsr4()
     {
         $loader = new Psr4ClassLoader;
-        $loader->addPrefix('MyBar\\', 'tests/fixtures/psr4/test/');
+        $loader->addPrefix('MyBar\\', 'tests/fixtures/class_loader/psr4/simple/');
         $classPath = $loader->resolveClass('MyBar\Foo');
         $this->assertNotNull($classPath);
         $this->assertFileExists($classPath);
